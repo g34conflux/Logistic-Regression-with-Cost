bf31364d4b0ac12c081edb3e6a4e7c5f5ed8985e
g34conflux <45349691+g34conflux@users.noreply.github.com>
2021-09-09 11:21:16 +0530

    Add files via upload

---
+++ plotData.m
@@ -0,0 +1,29 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+
+
+
+plot(X(find(y==1),1),X(find(y==1),2),'k+','LineWidth', 2, 'MarkerSize',6);
+
+plot(X(find(y==0),1),X(find(y==0),2), 'yo' ,'MarkerFaceColor', 'y', 'MarkerSize', 6);
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
